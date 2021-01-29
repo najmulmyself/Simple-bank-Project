5750ed5e1e9178323bfb234f221e6611c59b8460
Najmul Huda <najmul.myself@gmail.com>
2021-01-30 00:37:28 +0600

    Number add with function

--- script.js
+++ script.js
@@ -11,8 +11,19 @@ clickDepositButton.addEventListener('click',function(){
     const depositAmount = document.getElementById('depositAmount').value;
     const depositNumber = parseFloat(depositAmount);
 
-    const currentDepo = document.getElementById('depositText').innerText;
-    const currentDepoNumber = parseFloat(currentDepo);
-    const totalDepo  = depositNumber + currentDepoNumber;
-    document.getElementById('depositText').innerText = totalDepo;
+    // const currentDepo = document.getElementById('depositText').innerText;
+    // const currentDepoNumber = parseFloat(currentDepo);
+    // const totalDepo  = depositNumber + currentDepoNumber;
+    // document.getElementById('depositText').innerText = totalDepo;
+    document.getElementById('depositAmount').value = "";
+    updateSpanText('depositText',depositNumber)
+    updateSpanText('balanceText',depositNumber)
+
 });
+
+function updateSpanText(id , depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const total  = depositNumber + currentNumber;
+    document.getElementById(id).innerText = total;
+}
